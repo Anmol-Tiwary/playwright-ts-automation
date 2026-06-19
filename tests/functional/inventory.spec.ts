28d5a60a0d899d455614183dcf9c123a7a7b858f
Anmol Kumar <anmoltiwary4@gmail.com>
2026-06-19 16:00:51 +0530

    19th june inventory check and array

--- tests/functional/inventory.spec.ts
+++ tests/functional/inventory.spec.ts
@@ -14,7 +14,39 @@ test.describe("Inventory feature", () => {
         await expect(page).toHaveURL(/.*\/inventory/);
     });
 
-test("should validate the prie tag of product", async ({page}) => {
+    test("should validate the prie tag of product", async ({ page }) => {
+        // get a list of product
+        let prodEles = page.locator(".inventory_item");
+        await expect(prodEles).toHaveCount(6);
 
-})
+        //get prod name and prices
+        let totalProd = await prodEles.count();
+
+        let priceArr = []
+
+        for (let i = 0; i < totalProd; i++) {
+            let eleNode = prodEles.nth(i);
+
+            let prodName = await eleNode.locator(".inventory_item_name").innerText()
+            let price = await eleNode.locator(".inventory_item_price").innerText()
+
+            //Prinnt the result
+            console.log(`Products:  ${prodName}, Price: ${price}`)
+            priceArr.push(price)
+        }
+
+let priceArrNum = priceArr.map((item) => parseFloat(item.replace("$", "")))
+console.log(`>> Modified arr: ${priceArrNum}`)
+
+let zeroValueProd = priceArrNum.filter((item) => item <=0);
+
+if (zeroValueProd.length>0) {
+    console.log(`Error: Zero value prod found, ${zeroValueProd}`)
+} else {
+    console.log("all prod pricess are non zero");
+}
+
+expect(zeroValueProd).toHaveLength(0)
+
+    })
 });
