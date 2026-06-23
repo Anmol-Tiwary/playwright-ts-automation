63936dc4c3431cc37171ed4ddfc08dd3c37621d7
Anmol Kumar <anmoltiwary4@gmail.com>
2026-06-23 20:52:08 +0530

    Sync local changes and remove deleted files

--- tests/page-objects/nocommerce.home.page.ts
+++ tests/page-objects/nocommerce.home.page.ts
@@ -1,6 +1,7 @@
 import { expect, type Page } from "@playwright/test";
 import BasePage from "./base.page";
 import { log } from "../helpers/loggers";
+import { url } from "node:inspector";
 
 //Constructor
 class HomePage extends BasePage {
@@ -11,4 +12,19 @@ class HomePage extends BasePage {
     get userNameInputBox() {return this.page.getByRole ('textbox', {name: "Email:"})}
     get passwordInputBox() {return this.page.getByRole ('textbox', {name: "Password:"})}
     get loginBtn() {return this.page.getByRole ('button', {name: "Log in"})}
+
+
+//page actions
+async logIneCommApp( url: string, username: string, password: string){
+    await log("info", `login to the ${url}`);
+//Login
+await this.navigateTo(url)
+await this.typeInto(this.userNameInputBox, username)
+await this.typeInto(this.passwordInputBox, password)
+await this.click(this.loginBtn)
+
+//assertion
+await expect(this.page).toHaveURL(`1${url}/admin/`)
+await log("info", 'Homepage is sucessfully launched')
+}
 }
