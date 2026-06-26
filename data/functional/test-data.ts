42f60447f0f9b1fa94c9941626832e8e70588987
Anmol Kumar <anmoltiwary4@gmail.com>
2026-06-26 13:28:32 +0530

    complete restAPI test script

---
+++ data/functional/test-data.ts
@@ -0,0 +1,12 @@
+export default class TestData {
+
+static apiUsercreation() {
+    return[ {
+        "id": 125,
+        "email": "rachel.howell@reqres.in",
+        "first_name": "Rachelo",
+        "last_name": "Howelly",
+        "avatar": ""
+    }]
+}
+};
--- tests/api/users.api.spec.ts
+++ tests/api/users.api.spec.ts
@@ -1,19 +1,23 @@
 import { test, expect, request } from "@playwright/test"
 import { log } from "../helpers/loggers"
-import { json } from "node:stream/consumers"
+import constants from "../../data/functional/constants.json"
+import testData from "../../data/functional/test-data"
 
 test.describe("Rest Api Demo", () => {
-    const baseURL = "https://reqres.in/api"
-    //reqres.in/api/users?page=2
+
+    let envConfig = undefined
+    test.beforeEach("Load Env Config", async ({ request }, testInfo) => {
+        envConfig = testInfo.project.use as any
+    })
 
     // Get Method
     test("Should get list of Users", async ({ request }) => {
 
         //make a get call
-        await log("info", `Making a get call using ${baseURL}`)
-        const res = await request.get(`${baseURL}/users?page=2`, {
+        await log("info", `Making a get call using ${envConfig.apiURL}`)
+        const res = await request.get(`${envConfig.apiURL}${constants.REQ_REQ_ENDPOINTS.get_user}`, {
             headers: {
-                'x-api-key': 'free_user_3Fcnu9ODf0JXmOy3Sw7CMgmSwRw'
+                'x-api-key': process.env.apiKey
             }
         })
 
@@ -29,29 +33,23 @@ test.describe("Rest Api Demo", () => {
     //post method
     test("Should create a user", async ({ request }) => {
         //Make a Get call
-    await log("info", `Making a post call using ${baseURL}`)
+        await log("info", `Making a post call using ${envConfig.apiURL}`)
 
-    const payload = {
-       "id": 125,
-      "email": "rachel.howell@reqres.in",
-      "first_name": "Rachelo",
-      "last_name": "Howelly",
-      "avatar": "",
-    };
+        const payload = testData.apiUsercreation()[0]
 
-    const res = await request.post(`${baseURL}/users/2`, {
-        headers: {
-           'x-api-key': 'reqres_a44340d004184f328a40d5a5f5ba69de',
-           "Content-Type": "application/json",
-                   },
-                   data: payload
-    })
+        const res = await request.post(`${envConfig.apiURL}${constants.REQ_REQ_ENDPOINTS.POST_USER}`, {
+            headers: {
+                'x-api-key': process.env.apiKey,
+                "Content-Type": "application/json",
+            },
+            data: payload
+        })
 
-    //Assert the status code
-    expect(res.status()).toBe(201)
-    await log("info",` The post call is sucessfull ${res.status()}`)
+        //Assert the status code
+        expect(res.status()).toBe(201)
+        await log("info", ` The post call is sucessfull ${res.status()}`)
 
-    const restData =  await res.json();
-    log("info", ` Response data from the post call ${JSON.stringify(restData)}`)
+        const restData = await res.json();
+        log("info", ` Response data from the post call ${JSON.stringify(restData)}`)
     })
 })
